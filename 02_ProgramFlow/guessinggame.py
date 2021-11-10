aed92c63fc49bc8e4d7175f11509b8d1034ee373
Asato <asato_chen@outlook.com>
2021-11-10 17:48:53 +0800

    Fix a bug in guessinggame.py

--- 02_ProgramFlow/guessinggame.py
+++ 02_ProgramFlow/guessinggame.py
@@ -1,6 +1,7 @@
 import random
 
-def want_exit(number: str):
+
+def is_continued(number: str):
     return number.casefold() == exit_code
 
 
@@ -9,69 +10,69 @@ def is_valid_guess(number: str):
         print("Enter a 'NUMBER' between {0} and {1}."
               .format(min, max))
         return False
-    
+
     if not int(number) in range(min, max + 1):
         print("The answer is between {0} and {1}."
               .format(min, max))
         return False
-    
+
     return True
-    
+
 
 def give_hint(guess: int):
     if guess < answer:
         print("Guess higher (from {0} to {1})".format(guess, max))
         return (guess, max)
-    
+
     if guess > answer:
         print("Guess lower (from {0} to {1})".format(min, guess))
         return (min, guess)
-    
-    print("Congrats, you win!")
+
+    print("Congrats, you guessed the answer in {} moves!"
+          .format(guessed_times))
     return (min, max)
 
 
-def want_continue():
-    print("Wanna play again? (Press Y to restart)")
-    return input(">>  ").casefold() == "y"
-
-
 exit_code = "q"
-exit_game = False
+continued = True
 
-while not exit_game:
-    
+while continued:
+
     max = 100
     min = 1
     answer = random.randint(min, max)
     guess = ""
     guessed_times = 0
-    
+
     print("/" * 100)
-    print("\n*** The answer is {} ***\n".format(answer))      #TODO: Remove after testing 
+    # TODO: Remove after testing
+    print("\n*** The answer is {} ***\n".format(answer))
     print(
-        "Guess a number between {0} to {1} (Enter '{2}' to quit the game)"
+        "Guess a number between {0} and {1} (Enter '{2}' to quit the game)"
         .format(min, max, exit_code)
-        )
-            
+    )
+
     while guess != answer:
         guess = input(">>  ")
-        
-        if want_exit(guess):
-            exit_game = True
+
+        continued = is_continued(guess)
+        if not continued:
             break
-        
-        guessed_times += 1
-        
+
         if is_valid_guess(guess):
             guess = int(guess)
         else:
             continue
-        
+
+        guessed_times += 1
+
         min, max = give_hint(guess)
-        
+
     else:
-        exit_game = not want_continue()
-                    
+        continued = is_continued(
+            input(
+                """Wanna play again? (Press Q to exit)
+                >>  """))
+
 print("OK, bye.")
 print("/" * 100)

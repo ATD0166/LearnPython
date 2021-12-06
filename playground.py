ab49451b3713c3262de59a1425512760ff380a8f
Asato <asato_chen@outlook.com>
2021-12-06 20:50:39 +0800

    Practices in 12.6

--- playground.py
+++ playground.py
@@ -1,5 +1,29 @@
-import time
-print("Loading", end="")
-for i in range(6):
-    print(".", end='', flush=True)
-    time.sleep(0.2)
+def collatz(num: int) -> int:
+    number = int(num)
+    loop = 0
+    while number != 1:
+        loop += 1
+        if number % 2 == 0:
+            number = int(number / 2)
+        else:
+            number = number * 3 + 1
+    else:
+        # print(f"{num} loops {loop} times to 1")
+        return loop
+    
+    
+# number = input('Enter a natural number:\n')
+# with open('collatz_graph.txt', 'a') as text:
+#     for i in range(1, 10001):
+#         print(collatz(i), file=text)
+
+max = int(input())
+max_loop = 0
+print()
+for i in range(1, max + 1):
+    new_loop = collatz(i)
+    if new_loop > max_loop:
+        max_loop = new_loop
+        # print('|' * max_loop)
+        print(max_loop)
+    

d0c12fa239285f26d876ea5bcd7533ed75ef1260
Asato <asato_chen@outlook.com>
2021-12-08 22:23:08 +0800

    Lessons learned in 12.8

---
+++ 07_Module/browser.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+# webbrowser.open('https://www.python.org/')
+my_browser = webbrowser.get()
+my_browser.open('https://www.google.com/')
+print(my_browser)
---
+++ 07_Module/turtle_demo.py
@@ -0,0 +1,8 @@
+import turtle
+
+turtle.forward(150)
+turtle.right(180)
+turtle.circle(50)
+turtle.forward(150)
+
+turtle.done()
